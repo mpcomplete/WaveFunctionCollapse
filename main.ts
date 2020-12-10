5e7ce19aaa34b39ffee87428c88defa8a61e068f
Matt Perry <mpcomplete@gmail.com>
2020-12-09 21:38:14 -0500

    Add tileset parsing

--- main.ts
+++ main.ts
@@ -1,6 +1,6 @@
 import * as Dat from "dat.gui";
 
-let config = {};
+let config:any = {tmp:3};
 window.onload = function() {
   var gui = new Dat.GUI();
   const readableName = (n: string) => n.replace(/([A-Z])/g, ' $1').toLowerCase()
@@ -9,37 +9,41 @@ window.onload = function() {
     return gui.add(config, name, min, max).name(readableName(name));
   }
   addConfig("includeRotatedAndFlipped", false);
-  addConfig("tileSize", 3, 2, 10).step(1);
+  addConfig("tileSize", 3, 2, 10).step(1).onFinishChange(initTiles);
   addConfig("outputWidth", 20, 5, 1000).step(5);
   addConfig("outputHeight", 20, 5, 1000).step(5);
+  console.log("config done");
+
+  initTiles();
 };
 
-var img = new Image();
-img.crossOrigin = 'anonymous';
-img.src = "tiles.png";
+function initTiles() {
+  let img = new Image();
+  img.crossOrigin = 'anonymous';
+  img.src = "tiles.png";
 
-const $ = (id: string) => document.getElementById(id);
-
-img.onload = function() {
-  // Update the input tile display.
-  {
-    let canvas = $("input") as HTMLCanvasElement;
-    let context = canvas.getContext('2d');
-    if (!context) return;
-    context.imageSmoothingEnabled = false;
-    // [canvas.width, canvas.height] = [img.width, img.height];
-    context.drawImage(img, 0, 0, canvas.width, canvas.height);
-  }
-  // Get the image data from the image.
-  {
-    let canvas = new OffscreenCanvas(img.width, img.height);
-    let context = canvas.getContext('2d');
-    if (!context) return;
-    context.drawImage(img, 0, 0);
-    let imageData = context.getImageData(0, 0, img.width, img.height) as MyImageData;
-    parseImage(imageData);
-  }
-};
+  img.onload = function() {
+    console.log("image done");
+    // Update the input tile display.
+    {
+      let canvas = $("input") as HTMLCanvasElement;
+      let context = canvas.getContext('2d');
+      if (!context) return;
+      context.imageSmoothingEnabled = false;
+      // [canvas.width, canvas.height] = [img.width, img.height];
+      context.drawImage(img, 0, 0, canvas.width, canvas.height);
+    }
+    // Get the image data from the image.
+    {
+      let canvas = new OffscreenCanvas(img.width, img.height);
+      let context = canvas.getContext('2d');
+      if (!context) return;
+      context.drawImage(img, 0, 0);
+      let imageData = context.getImageData(0, 0, img.width, img.height) as MyImageData;
+      parseImage(imageData);
+    }
+  };
+}
 
 // How we identify a tile - just its index into the list of all tiles.
 type TileIndex = number;
@@ -50,22 +54,21 @@ type FrequencyWeight = number;
 // Cardinal directions.
 enum Direction {Up, Right, Down, Left};
 
-// An RGBA color. Range is 0-255.
-type Color = [number, number, number, number];
+// A 32-bit RGBA color. Format is 0xRRGGBBAA.
+type Color = number;
 
 // An x,y tuple.
 type Point = [number, number];
+type Size = [number, number];
 
 // Mapping of TileIndex -> color of the top-left pixel of that tile.
 type TileColorMapping = Color[];
 
+const $ = (id: string) => document.getElementById(id);
+
 // Manages relative frequencies of all the tiles. The higher a tile's frequency,
 // the more likely that tile will appear in the output.
 class FrequencyHints {
-  readonly _tileWeights: FrequencyWeight[];
-  readonly _sumWeight: FrequencyWeight;
-  readonly _sumWeightTimesLogWeight: number;
-
   // w[n] - the relative frequency of the given tile. This is simply the number of
   // times it appears in the source image.
   weightForTile(index: TileIndex): FrequencyWeight {
@@ -74,7 +77,11 @@ class FrequencyHints {
   // Cache of sum(w[n]), used in entropy calculations.
   get sumWeight() { return this._sumWeight; }
   // Cache of sum(w[n]*log(w[n])), used in entropy calculations.
-  get sumWeightTimesLogWeight() { return this._sumWeight; }
+  get sumWeightTimesLogWeight() { return this._sumWeightTimesLogWeight; }
+
+  private readonly _tileWeights: FrequencyWeight[];
+  private readonly _sumWeight: FrequencyWeight;
+  private readonly _sumWeightTimesLogWeight: number;
 }
 
 // Manages rules for which tiles are allowed to be adjacent to which other tiles, in a given direction.
@@ -91,12 +98,23 @@ interface MyImageData extends ImageData {
 }
 ImageData.prototype["get"] = function([x, y]: Point): Color {
   let r = 4 * (y * this.width + x);
-  return [this.data[r], this.data[r+1], this.data[r+2], this.data[r+3]];
+  return (this.data[r] << 24) | (this.data[r+1] << 16) | (this.data[r+2] << 8) | (this.data[r+3]);
+  // return [this.data[r], this.data[r+1], this.data[r+2], this.data[r+3]];
 }
 ImageData.prototype["set"] = function([x, y]: Point, color: Color): void {
   let r = 4 * (y * this.width + x);
-  for (let i = 0; i < 4; i++)
-    this.data[r+i] = color[i];
+  this.data[r+0] = (color >>> 24) & 0xff;
+  this.data[r+1] = (color >>> 16) & 0xff;
+  this.data[r+2] = (color >>> 8) & 0xff;
+  this.data[r+3] = (color) & 0xff;
+}
+
+function* rectRange([w, h]: Size) {
+  for (let y = 0; y < h; y++) {
+    for (let x = 0; x < w; x++) {
+      yield <Point>[x,y]
+    }
+  }
 }
 
 // var grayscale = function () {
@@ -113,5 +131,53 @@ ImageData.prototype["set"] = function([x, y]: Point, color: Color): void {
 //   ctx.putImageData(imageData, 0, 0);
 // };
 
+class Tile {
+  topLeftPixel(): Color { return this._pixels[0]; }
+  getPixel([x, y]: Point): Color { return this._pixels[x + y * config.tileSize]; }
+
+  static fromImageData(imageData: MyImageData, [ix, iy]: Point): Tile {
+    let tile = new Tile;
+    for (let [tx, ty] of rectRange([config.tileSize, config.tileSize]))
+      tile._pixels[tx + ty * config.tileSize] = imageData.get([(ix + tx) % imageData.width, (iy + ty) % imageData.height]);
+    return tile;
+  }
+
+  private _pixels: Color[] = [];
+}
+
 function parseImage(imageData: MyImageData) {
+  let tiles: Tile[] = [];
+  for (let p of rectRange([imageData.width, imageData.height])) {
+    let tile = Tile.fromImageData(imageData, p);
+    tiles.push(tile);
+  }
+
+  displayTileset(tiles);
+}
+
+function displayTileset(tiles: Tile[]) {
+  let across = Math.floor(Math.sqrt(tiles.length));
+  let down = Math.floor(tiles.length / across);
+  console.log(tiles.length, "=",config.tileSize, across, down);
+  let imageData = new ImageData((config.tileSize+1)*across - 1, (config.tileSize+1)*down - 1) as MyImageData;
+
+  for (let [i, j] of rectRange([across, down])) {
+    let tile = tiles[i + j*across];
+    for (let [x, y] of rectRange([config.tileSize, config.tileSize]))
+      imageData.set([(config.tileSize+1)*i + x, (config.tileSize+1)*j + y], tile.getPixel([x,y]));
+  }
+
+  // Blit to an offscreen canvas first, so we can scale it up when drawing it for real.
+  let buffer = new OffscreenCanvas(imageData.width, imageData.height);
+  {
+    let context = buffer.getContext('2d');
+    if (!context) return;
+    context.putImageData(imageData, 0, 0);
+  }
+
+  let canvas = $("tileset") as HTMLCanvasElement;
+  let context = canvas.getContext('2d');
+  if (!context) return;
+  context.imageSmoothingEnabled = false;
+  context.drawImage(buffer, 0, 0, canvas.width, canvas.height);
 }
